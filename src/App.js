cf6991de0e24cc0af8812c6159f3acf5c9d6e7e6
aymanom77 <aymanomais18@gmail.com>
2024-02-16 17:11:10 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    person: {
+      fullName: "John Doe",
+      bio: "entreprenant, polyvalant je suis pret a repondre aux exigance recquise pour passer le test.",
+      imgSrc: "https://franchisematch.com/wp-content/uploads/2015/02/john-doe.jpg",
+      profession: "Developer"
+    },
+    show: false,
+    interval: 0
+  };
+
+  componentDidMount() {
+    this.intervalId = setInterval(() => {
+      this.setState(prevState => ({ interval: prevState.interval + 1 }));
+    }, 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.intervalId);
+  }
+
+  toggleShow = () => {
+    this.setState(prevState => ({ show: !prevState.show }));
+  };
+
+  render() {
+    const { person, show, interval } = this.state;
+
+    return (
+      <div>
+        <button onClick={this.toggleShow}>Toggle Profile</button>
+        {show && (
+          <div>
+            <h1>{person.fullName}</h1>
+            <p>{person.bio}</p>
+            <img src={person.imgSrc} alt={person.fullName} />
+            <p>{person.profession}</p>
+          </div>
+        )}
+        <p>Elapsed time since mount: {interval} seconds</p>
+      </div>
+    );
+  }
 }
 
 export default App;
+
